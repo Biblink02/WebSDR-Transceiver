9d4b0fc923148be7a6a3bd99b380df0bb785b13b
achart <achart88@gmail.com>
2025-10-21 14:07:14 +0200

    test fusero

--- web-sdr/frontend/src/app/layouts/FooterLayout.vue
+++ web-sdr/frontend/src/app/layouts/FooterLayout.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-footer
+footer caca
 </template>
 
 <style scoped>
--- web-sdr/frontend/src/app/layouts/NavbarLayout.vue
+++ web-sdr/frontend/src/app/layouts/NavbarLayout.vue
@@ -2,9 +2,10 @@
 </script>
 
 <template>
-  <nav class="flex space-x-6 font-serif font-semibold text-gray-800 dark:text-gray-100 mb-6">
+  <nav class="flex space-x-6 font-serif font-semibold text-gray-800 dark:text-gray-100 mb-6 text-2xl">
     <router-link to="/" class="hover:underline">Our project</router-link>
     <router-link to="/sdr" class="hover:underline">SDR</router-link>
     <router-link to="/about" class="hover:underline">About us</router-link>
+    <router-link to="/about" class="hover:underline">Ressources</router-link>
   </nav>
 </template>
