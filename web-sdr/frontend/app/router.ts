6237d5fa24672d747a91d554cd7fa14b8b272404
Pitclair <fa.mahvari@gmail.com>
2025-10-30 16:51:29 +0100

    fix: resolved post-merge conflicts

--- web-sdr/frontend/app/router.ts
+++ web-sdr/frontend/app/router.ts
@@ -1,14 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import HomePage from './pages/HomePage.vue'
-import SrdPage from './pages/sdr/SdrPage.vue'
-import aboutpage from './pages/about.vue'
-import ressources from './pages/ressources.vue'
-import SatelliteTracker from './pages/SatelliteTracker.vue'
+import HomePage from '&/pages/HomePage.vue'
+import aboutpage from '&/pages/about.vue'
+import ressources from '&/pages/ressources.vue'
+import SatelliteTracker from '&/pages/SatelliteTracker.vue'
 
 
 const routes = [
     {path: '/', component: HomePage},
-    {path: '/sdr', component: SrdPage},
     {path: '/about', component: aboutpage},
     {path: '/ressources', component: ressources},
     {path: '/satellite-tracker', component: SatelliteTracker}
