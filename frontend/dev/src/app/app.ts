2c6807f11c429f91e9ce9f6a83ba1409f411b6a5
Biblink02 <alby.bot.24@gmail.com>
2025-12-04 21:47:25 +0100

    feat: toast from primevue

--- frontend/dev/src/app/app.ts
+++ frontend/dev/src/app/app.ts
@@ -7,10 +7,11 @@ import 'primeicons/primeicons.css'
 import 'devicon/devicon.min.css'
 import '@fontsource-variable/inter'
 import PrimeVue from 'primevue/config'
-import LocaleIt from 'primelocale/it.json'
+import LocaleEn from 'primelocale/en.json'
 import Aura from '@primevue/themes/aura'
 import {createHead} from '@unhead/vue/client'
 import {loadConfig} from "@/ConfigStore";
+import ToastService from 'primevue/toastservice';
 
 const initApp = async () => {
     await loadConfig()
@@ -26,10 +27,11 @@ const initApp = async () => {
                     darkModeSelector: ".dark",
                 },
             },
-            locale: LocaleIt['it'],
+            locale: LocaleEn['en'],
         })
+        .use(ToastService)
         .use(head)
         .mount('#app')
 }
 
-initApp().then()
+initApp()
