b3e0efe47be4bc6379b3779cad8adb9c04a9a4cd
Lorenzo Croce <lorenzo.croce@arenastudios.it>
2025-12-05 15:29:09 +0100

    refactor: small changes

--- frontend/dev/src/app/layouts/AppLayout.vue
+++ frontend/dev/src/app/layouts/AppLayout.vue
@@ -1,28 +1,22 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import NavbarLayout from './NavbarLayout.vue'
 import FooterLayout from './FooterLayout.vue'
 import ParticlesBackground from '../components/ParticlesBackground.vue'
-
 </script>
 
 <template>
-  <div class="min-h-screen w-full relative">
-    <!-- Fond animé -->
-    <ParticlesBackground class="absolute inset-0 z-0" />
+    <div class="min-h-screen w-full relative">
+        <ParticlesBackground class="absolute inset-0 z-0"/>
 
-    <!-- Contenu principal -->
-    <div class="relative z-10 max-w-8xl w-full mx-auto pt-4 p-1 sm:p-4 md:p-7 space-y-10 min-h-screen">
+        <div class="relative z-10 max-w-8xl w-full mx-auto pt-4 p-1 sm:p-4 md:p-7 space-y-10 min-h-screen">
 
-      <!-- Navbar -->
-      <NavbarLayout class="relative z-20" />
+            <NavbarLayout class="relative z-20"/>
 
-      <!-- Contenu des Pages -->
-      <slot />
+            <slot/>
 
-      <!-- Footer -->
-      <FooterLayout class="relative z-20" />
+            <FooterLayout class="relative z-20"/>
+        </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
